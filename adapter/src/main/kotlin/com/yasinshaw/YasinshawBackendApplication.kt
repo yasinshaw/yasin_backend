510501032d12ae83d87345879a2a6d8a4cd3b2ad
yasinshaw <yasinshaw@gmail.com>
2021-01-24 23:12:20 +0800

    set jasypt algorithm

--- adapter/src/main/kotlin/com/yasinshaw/YasinshawBackendApplication.kt
+++ adapter/src/main/kotlin/com/yasinshaw/YasinshawBackendApplication.kt
@@ -1,9 +1,11 @@
 package com.yasinshaw
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableEncryptableProperties
 class YasinshawBackendApplication
 
 fun main(args: Array<String>) {
